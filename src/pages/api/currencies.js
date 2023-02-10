6160e30f5485ca14de4db14d573346ba4498ceca
ChillFrog <kotila222@gmail.com>
2023-02-10 11:50:56 +0300

    added exchange rate

---
+++ src/pages/api/currencies.js
@@ -0,0 +1,30 @@
+export const currencies = [
+  {
+    name: "USD",
+    image: "https://flagicons.lipis.dev/flags/4x3/us.svg",
+  },
+  {
+    name: "EUR",
+    image: "https://flagicons.lipis.dev/flags/4x3/eu.svg",
+  },
+  {
+    name: "RUB",
+    image: "https://flagicons.lipis.dev/flags/4x3/ru.svg",
+  },
+  {
+    name: "JPY",
+    image: "https://flagicons.lipis.dev/flags/4x3/jp.svg",
+  },
+  {
+    name: "GBP",
+    image: "https://flagicons.lipis.dev/flags/4x3/gb.svg",
+  },
+  {
+    name: "CAD",
+    image: "https://flagicons.lipis.dev/flags/4x3/ca.svg",
+  },
+  {
+    name: "CNH",
+    image: "https://flagicons.lipis.dev/flags/4x3/cn.svg",
+  },
+];
